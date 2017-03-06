9cc5026f2be64960ccd5c0b372c6a96bee82021b
cfv7 <hq@colin.red>
2017-03-06 12:39:16 -0500

    quiz app first commit

---
+++ quizapp.js
@@ -0,0 +1,39 @@
+// names: Colin & Ally
+// date: 3.6.17
+// project: Quiz App
+
+//  REQUIREMENTS
+//  start screen w/ button click to start quiz
+//  question cards (div) forward arrow function
+//  question cards generate questions/ potential answers from object
+//  question cards output number of correct answers
+//  create correct or incorrect card background overlay
+//  questions cards number out of ten
+//  final card w/ number correct and start new game button
+
+//1- CREATE QUESTIONS OBJECT
+// Should have questions, answer choices, and correct answer
+
+//1b- CREATE STATE OBJECT
+// Create object for storing user's points, current question number e.g. 3/10
+
+//2- CREATE STATE MODIFICATION FUNCTIONS
+// Should have functions to add and update points and current question.
+// Functions: addPoints, updatePosition
+
+//3- CREATE RENDER / LOGIC FUNCTIONS
+// Draw Card - Render Questions to Dom.
+// Display what currentCard they are on.
+// Display numCorr and numIncorr.
+// Get usersAnswer
+// Check answer isCorrect
+// Display Response  "Correct / Incorrect" 
+// Display Correct Answer
+// Display nextButton.
+
+//4- CREATE EVENT LISTENERS
+// Display start card.
+// When users click on start button, advance to question card
+// Display the question card.
+// When users click on answer buttons,check if correct. display correct Answer. Show next button.
+
